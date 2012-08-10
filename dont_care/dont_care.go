70776ade7315f77ded9484d4d9d417c5b17e7fa9
Alejandro Cabrera <cpp.cabrera@gmail.com>
2012-08-10 05:52:38 -0400

    [src] Added dont_care range example.

---
+++ dont_care/dont_care.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	pow := make([]int, 10)
+	for i := range pow {
+		pow[i] = 1 << uint(i)
+	}
+
+	for _, value := range pow {
+		fmt.Println(value)
+	}
+}
